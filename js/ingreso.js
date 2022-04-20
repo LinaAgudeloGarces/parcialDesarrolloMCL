7424e1ccd11c7234e407a0aee2f9a45bebe3973d
LinaAgudeloGarces <linagub70@gmail.com>
2022-04-20 00:15:19 -0500

    Login contra encriptada y guia a página

--- js/ingreso.js
+++ js/ingreso.js
@@ -1,10 +1,16 @@
 //Variables que necesito
 let usuario = "susana.morera";
 let contrasena = "abcd1234";
+//Contraseña encriptada
+let contraEncriptada;
+let arregloDatos= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z","!","#","$","%","&","/","(",")","=","?","¡","¿","*","[","]","{","}","-","_","<",">",";"];
+//TAMAÑO ES 85
 
 //Variables que ingresa el usuario
 let usuarioIng;
 let contrasenaIng;
+
+
 
 //Para cuando unde boton
 let boton = document.getElementById("boton");
@@ -15,12 +21,16 @@ function ingresar(){
     //Obtengo los datos del usuario
     usuarioIng = document.getElementById("name").value;
     contrasenaIng = document.getElementById("contra").value;
-    console.log("Usuario: "+ usuarioIng + "\nContraseña: " + contrasenaIng);
+    
     //Saber si hay datos
     if((usuarioIng !="")&& (contrasenaIng !="")){
+        //Encriptar contraseña
+        contraEncriptada = encriptar(contrasenaIng);
+        console.log("Usuario: "+usuarioIng + "\nContraseña: "+ contrasenaIng + "\nENCRIPTADA: " + contraEncriptada );
         //Si el usuario y la contraseña son correctos
         if(usuarioIng===usuario&&contrasenaIng===contrasena){
-            window.location.href="./parcialDesarrolloMCL/panel.html";
+            //Abre la ventana
+            window.open("../panel.html");
         }
         //Si no es correcto el usuario
         else if(usuarioIng!=usuario&&contrasenaIng===contrasena){
@@ -32,9 +42,26 @@ function ingresar(){
         }
         //Si todo es malo
         else{
-            window.alert("El usuario y la contraseña no es correcto. Vuelve a comprobarlo");
+            window.alert("El usuario y la contraseña no son correctos. Vuelve a comprobarlos");
         }
     }
+}
+
+
+//Método que encripta la contraseña
+function encriptar(password){
+    let contraE = "";
+    //Vuelvo array la contraseña
+    let arregloPass = Array.from(password);
+    
+    let num=0;
+    for(let i=0; i<arregloPass.length; i++){
+        //Obtengo numero aleatorio
+        num = Math.round(Math.random() * (85));
+        contraE+= arregloDatos[num];
+    }
+    
+    return contraE;
 }
 
 //Método que se ejecuta al darle mostrar
